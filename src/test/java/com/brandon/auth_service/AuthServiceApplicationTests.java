7020f86f0f6119c95f3d71bb66846a482207b6bc
Brandon Mancia <bmancia001@gmail.com>
2026-03-09 19:38:25 -0500

    feat: initial project structure for JWT authentication service

---
+++ src/test/java/com/brandon/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandon.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
